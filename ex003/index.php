f623b3e931990f356e8a131144e185e646010a67
bioolegar1 <bioolegari@gmail.com>
2024-07-03 21:59:28 -0300

    Inserção de Tipos Primitivos com exemplo

---
+++ ex003/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos Primitivos</h1>
+
+    <?php 
+    //0x = Hexadecimal | 0b = Binário | 0 = Octal
+    // $num=010;
+    // echo"O valor da variável é $num";
+
+    // $v = "true";
+    // var_dump($v);
+
+    // $num = 3e2; // 3 * 10²
+    // echo"O valor é $num";
+
+    // $numIntForced = (int) 3e2; //coerção
+    // var_dump( $numIntForced );
+
+
+    // $num = (float)"950";
+    // var_dump($num);
+
+    // $casado = true;
+    // // var_dump( $casado );
+    // print"O valor para casado é $casado";
+
+
+    ?>
+</body>
+</html>
